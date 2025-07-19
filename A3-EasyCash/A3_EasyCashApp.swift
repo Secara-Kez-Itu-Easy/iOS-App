fc4de1453c71f91274da03d65c6017efa8c0930f
Azis <umar.santo1@gmail.com>
2025-07-20 04:18:19 +0700

    Initial Commit

---
+++ A3-EasyCash/A3_EasyCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  A3_EasyCashApp.swift
+//  A3-EasyCash
+//
+//  Created by Umar Abdul Azis on 20/07/25.
+//
+
+import SwiftUI
+
+@main
+struct A3_EasyCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
